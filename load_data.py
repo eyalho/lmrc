6e81bb95d4faf495f1573826a15d1d03872f7ae3
eyalho <eyal.res.dev@gmail.com>
2024-10-11 20:10:20 +0300

    clean up run.py

--- load_data.py
+++ load_data.py
@@ -1,4 +1,10 @@
+import os
+from functools import lru_cache
+from pathlib import Path
+
 import pandas as pd
+
+from evalute import calculate_wer, classify_location_error, print_error_analysis
 
 dataset_folder = "datasets"
 
@@ -11,8 +17,71 @@ def load_official():
     return official_test_data, official_training_data
 
 
+@lru_cache(maxsize=None)
 def load_labeled_test_data():
     labeled_test_data = pd.read_csv(f"{dataset_folder}/test_merged_df.csv")
     labeled_test_data = labeled_test_data.rename(columns={"location": "location_true"})
     labeled_test_data = labeled_test_data[labeled_test_data['location_mentions'].notna()]
     return labeled_test_data
+
+
+def create_a_submission_file(data, predict_func, save_path, exp=None):
+    print(f"shape of test_data: {data.shape}")
+
+    data['location'] = data['text'].apply(lambda x: predict_func(x))
+    os.makedirs(os.path.dirname(save_path), exist_ok=True)
+    data = data[["tweet_id", "location"]]
+    data.to_csv(save_path, index=False)
+    print(f"submission_file_path: {save_path}")
+    if exp:
+        exp.log_table(filename=save_path)
+
+    improved_test_data = data.merge(load_labeled_test_data(), on=['tweet_id'], how='left')
+    improved_test_data['location'] = improved_test_data.apply(
+        lambda row: row['location_true'] if pd.notna(row['location_true']) else row['location'], axis=1)
+    print(f"shape of improved_test_data: {improved_test_data.shape}")
+    # modify submission_file_path to be submission_file_path_with_true
+    submission_file_path_with_true = save_path.replace('.csv', '_with_true.csv')
+    improved_test_data[["tweet_id", "location"]].to_csv(submission_file_path_with_true, index=False)
+    print(f"submission_file_path_with_true: {submission_file_path_with_true}")
+    if exp:
+        exp.log_table(filename=submission_file_path_with_true)
+
+def load_and_evaluate_a_submission_file(submission_file_path, exp=None):
+    submission_file_name = Path(submission_file_path).name
+    eval_data = pd.read_csv(submission_file_path)
+    # set 2 first columns to tweet_id and location
+    if list(eval_data.columns) != ['tweet_id', 'location']:
+        print(f"Warnning {eval_data.columns}!=['tweet_id', 'location']")
+
+    eval_data.columns = ['tweet_id', 'location']
+    eval_data = eval_data.copy().merge(load_labeled_test_data(), on=['tweet_id'], how='left')
+    eval_data = eval_data[eval_data['location_true'].notna()]
+    eval_data['wer_score'] = eval_data.apply(lambda row: calculate_wer(row), axis=1)
+    # sum wer score and log it
+    average_test_wer = eval_data['wer_score'].mean()
+    print(f"Average WER: {average_test_wer}")
+    eval_data['location_error'] = eval_data.apply(
+        lambda row: classify_location_error(row['location_true'], row['location']), axis=1)
+
+    print_error_analysis(eval_data, by_location_errors=True)
+
+    eval_path = f'out/{submission_file_name}_eval.csv'
+    eval_data[["tweet_id", "wer_score", "location", "location_true", "location_error", "text"]].to_csv(eval_path,
+                                                                                                       index=False)
+    if exp:
+        exp.log_table(filename=eval_path)
+
+    error_analysis_path = f'out/{submission_file_name}_error_analysis.csv'
+    error_analysis_data = eval_data['location_error'].value_counts()
+    error_analysis_data.to_csv(error_analysis_path)
+    for error_type, count in error_analysis_data.items():
+        print(f"analysis:{error_type}: {count}")
+        if exp:
+            exp.log_metric(name=f"error_{error_type}", value=count)
+
+    return average_test_wer
+
+
+if __name__ == "__main__":
+    load_and_evaluate_a_submission_file("out/submission_0.235.csv")
--- run.py
+++ run.py
@@ -1,14 +1,12 @@
-import os
 import time
 
 import comet_ml
-import pandas as pd
 
-from evalute import print_error_analysis, classify_location_error, calculate_wer
-from load_data import load_official, load_labeled_test_data
+from load_data import load_official, load_labeled_test_data, load_and_evaluate_a_submission_file, \
+    create_a_submission_file
 from models.predefined_words import predefined_locations_predict
 
-EXP_NAME: str = "predefined_locations_predict_v4_threshold=5"
+EXP_NAME: str = "tmp123123"  # "predefined_locations_predict_v4_threshold=5"
 
 if EXP_NAME:
     print(f"Experiment name: {EXP_NAME}")
@@ -25,7 +23,6 @@ exp_name = exp.get_name()
 assert exp_name == EXP_NAME
 
 if __name__ == "__main__":
-
     ### load data
     official_test_data, official_training_data = load_official()
     labeled_test_data = load_labeled_test_data()
@@ -35,52 +32,17 @@ if __name__ == "__main__":
 
 
     ### Choose a predictor
-    # model which based only on known locations:
+    def predict(text):
+        # model which based only on known locations
+        return predefined_locations_predict(text, threshold=5)
 
-    ### Create a submission file
-    official_test_data['location'] = official_test_data['text'].apply(lambda x: predefined_locations_predict(x, threshold=5))
+
+    ### Create a submission file (and enhanced file with true locations)
     submission_file_path = f'out/{exp_name}_submission.csv'
-    os.makedirs(os.path.dirname(submission_file_path), exist_ok=True)
-    official_test_data[["tweet_id", "location"]].to_csv(submission_file_path, index=False)
-
-    #### Improve submission by true location if available
-    improved_test_data = official_test_data.copy().merge(labeled_test_data, on=['tweet_id', 'text'], how='left')
-    improved_test_data['location'] = improved_test_data.apply(
-        lambda row: row['location_true'] if pd.notna(row['location_true']) else row['location'], axis=1)
-    print(f"shape of test_data: {improved_test_data.shape}")
-    # modify submission_file_path to be submission_file_path_with_true
-    submission_file_path_with_true = submission_file_path.replace('.csv', '_with_true.csv')
-    improved_test_data[["tweet_id", "location"]].to_csv(submission_file_path_with_true, index=False)
+    create_a_submission_file(official_test_data, predict, submission_file_path)
 
     ### Eval base on true location test set
-    eval_data = official_test_data.copy().merge(labeled_test_data, on=['tweet_id', 'text'], how='left')
-    eval_data = eval_data[eval_data['location_true'].notna()]
-    print(f"shape of eval_data: {eval_data.shape}")
-
-    # calculate WER score
-    eval_data['wer_score'] = eval_data.apply(lambda row: calculate_wer(row), axis=1)
-    # sum wer score and log it
-    average_test_wer = eval_data['wer_score'].mean()
-    print(f"Average WER: {average_test_wer}")
-    exp.log_metric(name="test_wer", value=average_test_wer)
-
-    # apply classify_location_error on each row of eval_data and add column location_error
-    eval_data['location_error'] = eval_data.apply(
-        lambda row: classify_location_error(row['location_true'], row['location']), axis=1)
-
-    print_error_analysis(eval_data, by_location_errors=True)
-
-    eval_path = f'out/{exp_name}_eval.csv'
-    eval_data[["tweet_id", "wer_score", "location", "location_true", "location_error", "text"]].to_csv(eval_path, index=False)
-    exp.log_table(filename=eval_path)
-
-    error_analysis_path = f'out/{exp_name}_error_analysis.csv'
-    error_analysis_data = eval_data['location_error'].value_counts()
-    error_analysis_data.to_csv(error_analysis_path)
-    exp.log_table(filename=error_analysis_path)
-    for error_type, count in error_analysis_data.items():
-        print(f"analysis:{error_type}: {count}")
-        exp.log_metric(name=f"error_{error_type}", value=count)
+    load_and_evaluate_a_submission_file(submission_file_path, exp)
 
 # Some other Links:
 # Evalution with https://github.com/rsuwaileh/seqeval
